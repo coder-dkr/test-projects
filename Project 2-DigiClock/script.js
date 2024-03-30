13c3f174d2f76ef455e8836490bfab5a7bc33100
Dhruv Roy <dhruvakr9001@gmail.com>
2024-03-31 03:13:23 +0530

    fixed for netlify deploy

--- Project 2-DigiClock/script.js
+++ Project 2-DigiClock/script.js
@@ -18,47 +18,51 @@ setInterval(() => {
     document.getElementById('time').innerHTML = `<b> ${time} on     ${dates} </b>`
 }, 100);
 
+function isYesMatch(value) {
+    yesArr = ['y', 'yes','I pay my respects to grand overseer'];
+    return yesArr.some((element) => {
+        return element.toLowerCase() === value.toLowerCase();
+    })
+}
+function isNoMatch(value) {
+    noArr = ['n', 'no','nah'];
+    return noArr.some((element) => {
+        return element.toLowerCase() === value.toLowerCase();
+    })
+}
+
 const myask = () => {
-    
-    yesArr = ['y', 'Y', 'YES', 'yes'];
-    noArr = ['n', 'N', 'NO', 'no'];
-    
+    alert("HOHOHO, YOU ARE HERE...GREETINGS MORTAL, I\'M..THE HEAVENLY CREATOR ~ OVERSEER(ROY)");
+    let firs = prompt("(will you greet the divine overseer?)", 'I pay my respects to grand overseer/nah');
+    if (isYesMatch(firs)) {
+        alert("HOHOHOHO WHAT A WELL MANNERED CHILD..")
+        alert("IN THE CURRENT ERA, COUNTLESS MORTALS HAVE BEEN SWAYED BY DEMON OF LAZE..BUT YOUR AURA IS DIFFERENT..")
+        firs = prompt("O MORTAL...DO YOU..DESIRE GREATNESS ? ", 'y/n')
+        if (isYesMatch(firs)) {
+            firs = prompt("OH YOURE AN INTERESTING ONE, BUT ARE YOU SURE..? THE PATH YOU'LL TAKE TRANSCENDS MORTAL LIMITS..", 'y/n')
+            if (isYesMatch(firs)) {
 
-    alert("HOHOHO, YOU ARE HERE...GREETINGS MORTAL, I\'M..THE HEAVENLY CREATOR ~ OVERSEER(");
-    let firs = prompt("(will you greet the divine overseer?)", 'y/n');
-    let isYesMatch = yesArr.some((element) => {
-        return element == firs;
-    })
-    let isNoMatch = noArr.some((element) => {
-        return element == firs;
-    })
-    if (isYesMatch) {
-
-        firs = prompt("DO YOU..DESIRE GREATNESS", 'y/n')
-
-        if (isYesMatch) {
-            firs = prompt("OH YOURE BRAVE BUT ARE YOU SURE..? THE PATH YOU'LL TAKE TRANSCENDS MORTAL LIMITS..", 'y/n')
-            if (isYesMatch) {
+                alert("KWAHAHAHAHA ITS VERY RARE TO ENCOUNTER A STRONG WILLED MORTAL LIKE YOU..IM IMPRESSED")
                 alert("VERY WELL...MAY LUCK BE WITH YOU...!!!")
                 location.href = "https://www.google.com/search?q=how+to+achieve+greatness";
             }
-            else if (isNoMatch) {
-                alert("NO MATTER...NOT EVERYONE IS DESTINED TO BE GREAT..")
+            else if (isNoMatch(firs)) {
+                alert("SUCH WEAK WILL...[-_-] NO MATTER...NOT EVERYONE IS DESTINED TO BE GREAT..")
             }
             else {
                 alert("WEAKONE..YOU LACK FOCUS...TRY AGAIN")
             }
         }
-        else if (isNoMatch) {
+        else if (isNoMatch(firs)) {
             firs = prompt(" ARE YOU SURE MORTAL??", 'y/n')
-            if (isYesMatch) {
-                firs = prompt("Pakka bro?? uh -cough-I MEAN YOURE 100% NOT INTERESTED..???")
-                if (isYesMatch) {
+            if (isYesMatch(firs)) {
+                firs = prompt("Pakka bro?? uh, I MEAN YOURE 100% NOT INTERESTED..???")
+                if (isYesMatch(firs)) {
                     alert('I SEE, YOU WERE NOT THE CHOOSEN ONE')
                 }
-                else if (isNoMatch) {
+                else if (isNoMatch(firs)) {
                     firs = prompt("HMMMM...WILL YOUU CONTINUE THEN", 'y/n')
-                    if (isYesMatch) {
+                    if (isYesMatch(firs)) {
                         alert("HAHAHAHAHAHA SO YOU ARE NOT A COMPLETE TRASH...")
                         alert("VERY WELL...MAY LUCK BE WITH YOU...!!!")
                         location.href = "https://www.google.com/search?q=how+to+achieve+greatness";
@@ -68,35 +72,24 @@ const myask = () => {
                     }
                 }
             }
-            else if (isNoMatch) {
+            else if (isNoMatch(firs)) {
                 firs = prompt("EVERSINCE THIS WORLD CAME TO BE..ALL MORTALS HAVE CHOOSEN THEIR UNIQUE 'PATH OF LIFE'...WILL YOU CARRY THE WILL OF YOUR ANCESTORS AND STRIVE TO TRANSCEND??", 'y/n')
-                if (isYesMatch) {
+                if (isYesMatch(firs)) {
                     alert("HAHAHAHA BRAVOOOO SO YOU DO HAVE WHAT IT TAKES...IM SATISFIED WITH YOUR RESPONSE")
                     alert("VERY WELL...MAY LUCK BE WITH YOU...!!!")
                     location.href = "https://www.google.com/search?q=how+to+achieve+greatness";
-
                 }
                 else {
                     alert("WEAKNESS DISGUST ME [-_-]")
                 }
             }
-
         }
         else {
             alert("SO YOU WERE NEVER AWARE..NAIVE..YOU MAY LEAVE")
         }
-
     }
     else {
         alert("[-_-] NAIVE CHILD...BEFORE ANYTHING..LEARN SOME-")
         location.href = "https://www.google.com/search?q=manners";
-
     }
-
-
-
-
-
 }
-
-
