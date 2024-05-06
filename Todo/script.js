170979b6d9bcd91e96b5b1c142372b5d1954ccda
Dhruv Roy <dhruvakr9001@gmail.com>
2024-05-06 17:39:09 +0530

    todo edi del

--- Todo/script.js
+++ Todo/script.js
@@ -12,11 +12,12 @@ let taskInp = document.getElementById("add-task")
 let popup = document.getElementById("popup")
 let form = document.getElementById("form-card")
 let formBtn = document.getElementById("form-button")
+let updateFormBtn = document.getElementById("Update-form-button")
 let newTask = document.querySelector(".new-card")
 
 let primaryCard = document.querySelector(".Primary-card")
 
-
+let cardfoots = document.querySelectorAll(".cardfoot")
 
 addBtn.addEventListener("click", (e) => {
     titleInp.value = ""
@@ -26,28 +27,63 @@ addBtn.addEventListener("click", (e) => {
 
 
 
+
 })
 closePop.addEventListener("click", () => {
-    titleInp.value = "";
-    taskInp.value = "";
     popup.style.display = "none";
 })
 
 formBtn.addEventListener("click", (e) => {
-    e.preventDefault()
-    let title = titleInp.value;
-    let task = taskInp.value;
-    popup.style.display = "none"
-    let dateLog = `${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`
-    primaryCard.insertAdjacentHTML('afterend',`  <li class="card new-card">
-                
+    if (titleInp.value === "" || taskInp.value === "") {
+        alert("TASK CANNOT BE EMPTY");
+        e.preventDefault()
+    }
+    else {
+        e.preventDefault()
+
+        let title = titleInp.value;
+        let task = taskInp.value;
+        popup.style.display = "none"
+        let dateLog = `${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`
+        primaryCard.insertAdjacentHTML('afterend', `
+<li class="card new-card">
     <span class="title">${title}</span>
     <span class="note">${task}</span>
     <hr class="line">
-    <span class="date">${dateLog}</span>
+    <div class="cardfoot">
+        <span class="date">${dateLog}</span>
+        <span class="editdel">
+          <span class="edit"><img onclick="editTask(this)" src="img/pen-solid.svg" alt="pen"></span>
+          <span class="delnote"><img onclick="deleteTask(this)" src="img/trash-can-solid.svg" alt="pen">
+        </span>
+        </span>
+    </div>
 
 </li>`);
 
+        let edits = document.querySelectorAll(".edit")
+        Array.from(edits).forEach(e => {
+            e.addEventListener("mouseover", () => {
+                e.parentElement.parentElement.classList.add("activeedit")
+            })
+            e.addEventListener("mouseout", () => {
+                e.parentElement.parentElement.classList.remove("activeedit")
+            })
+
+        })
+        let delnotes = document.querySelectorAll(".delnote")
+        Array.from(delnotes).forEach(e => {
+            e.addEventListener("mouseover", () => {
+                e.parentElement.parentElement.classList.add("activedel")
+            })
+            e.addEventListener("mouseout", () => {
+                e.parentElement.parentElement.classList.remove("activedel")
+            })
+
+        })
+
+
+    }
 
 })
 
@@ -59,36 +95,38 @@ delBtn.addEventListener("click", (e) => {
 })
 
 
+const deleteTask = (e) => {
+    let delconfirm = confirm("Delete this task?")
+    if(delconfirm){
+        e.parentElement.parentElement.parentElement.parentElement.remove()
+    }
+    else{
+        console.warn("TASK NOT DELETED")
+    }
+}
 
-
-
-
-    // let div1 = document.createElement("div");
-    // div1.className = "card"
-    // div1.classList.add("new-card")
+let editTask = (e)=>{
+    popup.style.display = "block"
+    formBtn.style.display = "none"
+    updateFormBtn.style.display="block"
+   let editableTitle = e.parentElement.parentElement.parentElement.previousElementSibling.previousElementSibling.previousElementSibling;
+    let editableTask = e.parentElement.parentElement.parentElement.previousElementSibling.previousElementSibling;
+    taskInp.value = editableTask.innerHTML
+    titleInp.value = editableTitle.innerHTML
+  console.log("hei")
     
-    // let div2 =document.createElement("div")
-    // div2.className = "task" 
-    
-    // let span1 = document.createElement("span")
-    // span1.className = "title"
-    // span1.innerHTML = titleInp.value
-    
-    // let hr = document.createElement("hr") 
-    // hr.className = "line"
-    
-    // let span2 = document.createElement("span")
-    // span2.className = "note"
-    // span2.innerHTML = taskInp.value
-    
-    // let span3 = document.createElement("span")
-    // span3.className = "date"
-    // span3.innerHTML = `${dateLog}`
-    
-    // div1.appendChild(div2)
-    
-    // div2.appendChild(span1)
-    // div2.appendChild(hr)
-    // div2.appendChild(span2)
-    
-    // div1.appendChild(span3)
+        updateFormBtn.addEventListener("click",(p)=>{
+            p.preventDefault()
+            if(titleInp.value === "" || taskInp.value === ""){
+                alert("TASK CANNNOT BE EMPTY")
+            }
+            else{
+            editableTitle.innerHTML = titleInp.value
+            editableTask.innerHTML = taskInp.value
+            formBtn.style.display = "block"
+            updateFormBtn.style.display="none"
+            popup.style.display = "none"
+            }
+            
+        })           
+}
