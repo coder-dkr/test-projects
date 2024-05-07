f4d373dbf3de896897ac2ba06e40891955e5f527
Dhruv Roy <dhruvakr9001@gmail.com>
2024-05-07 20:07:55 +0530

    Everything working

--- Todo/script.js
+++ Todo/script.js
@@ -3,23 +3,18 @@ let addBtn = document.querySelector(".addBtn")
 let delBtn = document.querySelector(".delBtn")
 let closePop = document.getElementById("closepopBtn")
 const todoKey = "todoData";
-
 // adding task to grid
 let taskTitle = document.querySelector(".title")
 let taskNote = document.querySelector(".note")
 let titleInp = document.getElementById("add-title")
 let taskInp = document.getElementById("add-task")
-
 let popup = document.getElementById("popup")
 let form = document.getElementById("form-card")
 let formBtn = document.getElementById("form-button")
 let updateFormBtn = document.getElementById("Update-form-button")
 let newTask = document.querySelector(".new-card")
-
 let primaryCard = document.querySelector(".Primary-card")
-
 let cardfoots = document.querySelectorAll(".cardfoot")
-
 addBtn.addEventListener("click", (e) => {
     titleInp.value = ""
     taskInp.value = ""
@@ -27,13 +22,11 @@ addBtn.addEventListener("click", (e) => {
     popup.style.display = "block";
 
 })
-
 closePop.addEventListener("click", () => {
     popup.style.display = "none";
 })
 let i = 0
 formBtn.addEventListener("click", (e) => {
-
     if (titleInp.value === "" || taskInp.value === "") {
         alert("TASK CANNOT BE EMPTY");
         e.preventDefault()
@@ -56,9 +49,7 @@ formBtn.addEventListener("click", (e) => {
             temp.push(Data)
             localStorage.setItem(todoKey, JSON.stringify(temp))
         }
-
         popup.style.display = "none"
-
         primaryCard.insertAdjacentHTML('afterend', `
         <li class="card new-card">
         <span class="title forEDITtit">${Data.title}</span>
@@ -67,17 +58,13 @@ formBtn.addEventListener("click", (e) => {
         <div class="cardfoot">
         <span class="date">${Data.dateLog}</span>
         <span class="editdel">
-        <span class="edit"><img onclick="editTask(this)" src="img/pen-solid.svg" alt="pen"></span>
+        <span class="edit"><img onclick="editTask(this,${Data.id})" src="img/pen-solid.svg" alt="pen"></span>
         <span class="delnote"><img onclick="deleteTask(this,${Data.id})" src="img/trash-can-solid.svg" alt="pen">
         </span> 
         </span>
         </div>
-        
         </li>` );
- 
-
         i++;
-
         let edits = document.querySelectorAll(".edit")
         Array.from(edits).forEach(e => {
             e.addEventListener("mouseover", () => {
@@ -86,7 +73,6 @@ formBtn.addEventListener("click", (e) => {
             e.addEventListener("mouseout", () => {
                 e.parentElement.parentElement.classList.remove("activeedit")
             })
-
         })
         let delnotes = document.querySelectorAll(".delnote")
         Array.from(delnotes).forEach(e => {
@@ -96,16 +82,12 @@ formBtn.addEventListener("click", (e) => {
             e.addEventListener("mouseout", () => {
                 e.parentElement.parentElement.classList.remove("activedel")
             })
-
         })
         localStorage.setItem("number", i)
-
     }
-
 })
 let todoDataList = JSON.parse(localStorage.getItem(todoKey))
 if (todoDataList !== null) {
-
     todoDataList.forEach((elem, id) => {
         primaryCard.insertAdjacentHTML('afterend', `
         <li class="card new-card">
@@ -115,12 +97,11 @@ if (todoDataList !== null) {
         <div class="cardfoot">
         <span class="date">${elem.dateLog}</span>
         <span class="editdel">
-        <span class="edit"><img onclick="editTask(this)" src="img/pen-solid.svg" alt="pen"></span>
+        <span class="edit"><img onclick="editTask(this,${elem.id})" src="img/pen-solid.svg" alt="pen"></span>
         <span class="delnote"><img onclick="deleteTask(this,${elem.id})" src="img/trash-can-solid.svg" alt="pen">
         </span>
         </span>
         </div>
-        
         </li>` );
 
     })
@@ -134,7 +115,6 @@ Array.from(edits).forEach(e => {
     e.addEventListener("mouseout", () => {
         e.parentElement.parentElement.classList.remove("activeedit")
     })
-
 })
 let delnotes = document.querySelectorAll(".delnote")
 Array.from(delnotes).forEach(e => {
@@ -144,123 +124,73 @@ Array.from(delnotes).forEach(e => {
     e.addEventListener("mouseout", () => {
         e.parentElement.parentElement.classList.remove("activedel")
     })
-
 })
 //&&&&&&&&&&&&&&&&
-
-delBtn.addEventListener("click", (e) => {
-    e.preventDefault()
-    localStorage.clear()
-    location.reload()
-
-
-})
-
-
 const deleteTask = (e, dataId) => {
-
     let delconfirm = confirm("Delete this task?")
     if (delconfirm) {
         let tempDataList = JSON.parse(localStorage.getItem(todoKey))
         if (tempDataList != null) {
-
             tempDataList.forEach((value, index) => {
                 if (dataId == value.id) {
-                    tempDataList.splice(index, 1)
+                    tempDataList.splice(index, 1,)
                     localStorage.setItem(todoKey, JSON.stringify(tempDataList));
                     console.log("puchii")
                 }
             })
-
             e.parentElement.parentElement.parentElement.parentElement.remove()
         }
-
     }
     else {
         console.warn("TASK NOT DELETED")
     }
 }
 
-function editTask(e) {
+function editTask(e, editId) {
     popup.style.display = "block"
     formBtn.style.display = "none"
-    updateFormBtn.style.display = "block"
-    //    let editableTitle = e.parentElement.parentElement.parentElement.previousElementSibling.previousElementSibling.previousElementSibling;
-    //     let editableTask = e.parentElement.parentElement.parentElement.previousElementSibling.previousElementSibling;
-    let editableTitle = e.parentElement.parentElement.parentElement.parentElement.getElementsByClassName("forEDITtit")[0];
-    let editableTask = e.parentElement.parentElement.parentElement.parentElement.getElementsByClassName("forEDITtas")[0];
-
+    updateFormBtn.style.display = "block";
+    let editableTitle = e.parentElement.parentElement.parentElement.previousElementSibling.previousElementSibling.previousElementSibling;
+    let editableTask = e.parentElement.parentElement.parentElement.previousElementSibling.previousElementSibling;
     taskInp.value = editableTask.innerHTML
     titleInp.value = editableTitle.innerHTML
-    console.log(editableTitle)
-    console.log(editableTask)
 
-
-    console.log("before click")
     updateFormBtn.addEventListener("click", (p) => {
         p.preventDefault()
         if (titleInp.value === "" || taskInp.value === "") {
             alert("TASK CANNNOT BE EMPTY")
         }
         else {
-            console.log(editableTitle)
-            console.log(editableTask)
-            editableTitle.innerHTML = titleInp.value
-            editableTask.innerHTML = taskInp.value
+            let dateoftask = e.parentElement.parentElement.previousElementSibling.innerHTML
+
+            let EditedData = {
+                "title": `${titleInp.value}`,
+                "task": `${taskInp.value}`,
+                "dateLog": `${dateoftask}`,
+                "id": `${editId}`
+            };
+
+            let tempDataList = JSON.parse(localStorage.getItem(todoKey))
+            if (tempDataList != null) {
+                tempDataList.forEach((value, index) => {
+                    if (editId == value.id) {
+                        tempDataList.splice(index, 1, EditedData)
+                        localStorage.setItem(todoKey, JSON.stringify(tempDataList));
+                    }
+                })
+            }
             formBtn.style.display = "block"
             updateFormBtn.style.display = "none"
             popup.style.display = "none"
+            location.reload()
         }
-
     })
 }
+delBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    localStorage.clear()
+    location.reload()
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//alt method yet to try
-
-
-
-// const storedTitles = localStorage.getItem("title");
-// const storedTasks = localStorage.getItem("task");
-
-// const titlesArray = storedTitles.split(",");
-// const tasksArray = storedTasks.split(",");
-
-
-// const primaryCard = document.querySelector(".Primary-card");
-
-
-// for (let i = 0; i < titlesArray.length; i++) {
-//     const newCard = document.createElement("li");
-//     newCard.classList.add("card", "new-card");
-//     newCard.innerHTML = `
-//         <span class="title">${titlesArray[i]}</span>
-//         <span class="note">${tasksArray[i]}</span>
-//         <!-- Other card elements (date, edit/delete buttons, etc.) -->
-//     `;
-//     primaryCard.appendChild(newCard);
-// }
 
 
