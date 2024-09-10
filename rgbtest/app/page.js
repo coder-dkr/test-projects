89de5e1a4e9a3aff492445e6a15c101aa03240b0
Dhruv Roy <dhruvakr9001@gmail.com>
2024-09-10 10:18:01 +0530

    rgbtest

---
+++ rgbtest/app/page.js
@@ -0,0 +1,22 @@
+import Script from 'next/script';
+import '@/js/style.css'; // Adjust this path according to your styles structure
+
+export default function Home() {
+  return (
+    <>
+      <section className="h-screen w-screen">
+        <div id="rgbKineticSlider" className="rgbKineticSlider"></div>
+        <nav>
+          <a href="#" className="maib-nav prev" data-nav="prev">Prev</a>
+          <a href="#" className="maib-nav next" data-nav="next">Next</a>
+        </nav>
+      </section>
+
+      {/* Load scripts from the public directory */}
+      <Script src="/js/TweenMax.min.js" strategy="beforeInteractive" />
+      <Script src="/js/pixi.min.js" strategy="beforeInteractive" />
+      <Script src="/js/rgbKineticSlider.js" strategy="afterInteractive" />
+      <Script src="/js/script.js" strategy="afterInteractive" />
+    </>
+  );
+}
